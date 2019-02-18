d2c74f336829d956dc6776d6ca7158eb9a22dcaa
Shafeer Puthalan <shafeer.puthalan@cowrks.com>
2019-02-18 21:03:14 +0530

    Initial Commit

---
+++ SwiftyDS/SwiftyDS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDS.h
+//  SwiftyDS
+//
+//  Created by Shafeer Puthalan on 18/02/19.
+//  Copyright © 2019 Shafeer Puthalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDS.
+FOUNDATION_EXPORT double SwiftyDSVersionNumber;
+
+//! Project version string for SwiftyDS.
+FOUNDATION_EXPORT const unsigned char SwiftyDSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDS/PublicHeader.h>
+
+
